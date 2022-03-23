775975be3efc5c48716b786ebd339e9cd5952eed
root <abiol4001@gmail.com>
2022-03-22 19:48:58 -0700

    Reverse string

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -6,7 +6,7 @@
  * @b: the second integer
  */
 
-void swap_int(int *a, int*b)
+void swap_int(int *a, int *b)
 {
 	int c = *a;
 
---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ * followed by a new line
+ * @s: pointer string
+ * Return: no return
+ */
+
+void print_rev(char *s)
+{
+	int count = 0, i, j;
+	char *str, temp;
+
+	while (count >= 0)
+	{
+		if (s[count] == '\0')
+			break;
+		count++;
+	}
+	str = s;
+
+	for (i = 0; i < (count - i); i++)
+	{
+		for (j = i + 1; j > 0; j--)
+		{
+			temp = *(str + j);
+			*(str + j) = *(str + (j - 1));
+			*(str + (j - 1)) = temp;
+		}
+	}
+}
