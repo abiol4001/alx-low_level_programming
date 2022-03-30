4183f106ede4e70f282d252457f54adb75b434f3
root <abiol4001@gmail.com>
2022-03-29 19:34:00 -0700

    Locates a substring

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * strpbrk - searches a string for any of a set of bytes
+ * _strpbrk - searches a string for any of a set of bytes
  * @s: first string
  * @accept: second string
  * Return: a pointer to the byte in s that matches one of
